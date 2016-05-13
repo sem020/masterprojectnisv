91f4af078f3790ee30e7abfa29129566f4300d9e
sem020 <stephanm_91@hotmail.com>
2016-05-13 10:54:56 +0200

    parse transcriptions added, remove stopwords added, create baseline

--- parse_transcriptions.py
+++ parse_transcriptions.py
@@ -1,9 +1,14 @@
 import xml.etree.cElementTree as ET
 tree = ET.parse('transcriptions/transcription1.xml')
 root = tree.getroot()
-print root.tag
+#print root.tag
+transcription = ''
 for elem in tree.iter():
-    print elem.tag, elem.attrib
-for elem in tree.iter(tag='wordsequence'):
-    print elem.text
-print 'imaz'
+    #print elem.tag
+    if elem.tag == '{http://instituut.beeldengeluid.nl/namespace/iMMix}wordsequence':
+        if elem.text is not None and elem.text.strip():
+            transcription += elem.text + ' '
+            #print '%s' % elem.text
+def parsetranscriptions():
+    return transcription
+
--- stopwords.py
+++ stopwords.py
@@ -1,10 +1,10 @@
-query = 'What is hello if ed een hier hierin i can is isb'
-file = 'dutch_stopwords.txt'
-with open(file) as f:
-    lines = [line.rstrip('\n') for line in open(file)]
-querywords = query.split()
+def removestopwords(query):
 
-resultwords  = [word for word in querywords if word.lower() not in lines]
-result = ' '.join(resultwords)
+    file = 'dutch_stopwords.txt'
+    with open(file) as f:
+        lines = [line.rstrip('\n') for line in open(file)]
+    querywords = query.split()
 
-print result
+    resultwords  = [word for word in querywords if word.lower() not in lines]
+    result = ' '.join(resultwords)
+    return result
